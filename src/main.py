651164c1a00cd447650acd0a7ed1d156ebde69c1
Denis Miroshnik <kanokito@gmail.com>
2026-01-28 17:21:23 +0300

    ruff fix

--- src/main.py
+++ src/main.py
@@ -1,10 +1,10 @@
-# src/main.py
 import sys
 from pathlib import Path
 
 sys.path.insert(0, str(Path(__file__).parent))
 
 from loguru import logger
+
 from api.fastapi_app import get_fastapi_app
 from core.config import BASE_DIR, settings
 
@@ -30,13 +30,11 @@ def configure_logger() -> None:
     )
 
 
-# Важно: приложение создаётся на уровне модуля
 app = get_fastapi_app()
 
 if __name__ == '__main__':
     configure_logger()
     logger.info("Запуск приложения через if __name__ == '__main__'")
-    # Можно оставить для ручного запуска python src/main.py
     import uvicorn
 
     uvicorn.run(
